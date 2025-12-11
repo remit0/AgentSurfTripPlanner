59e13aa21c9a3475b364620ee74c77cc859dd344
Remi Rosenthal <remi-rosenthal@hotmail.fr>
2025-12-11 15:56:43 +0100

    Version 1.0 before refacto

--- scripts/main.py
+++ scripts/main.py
@@ -2,35 +2,54 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-
 import dataiku
-from langchain_core.messages import HumanMessage
+from dataiku.langchain import LangchainToDKUTracer
+from dataiku.llm.python import BaseLLM
+from langchain_core.messages import AIMessage, HumanMessage
 from surf_planner.build import AgentBuilder
 from surf_planner.models import DKUChatLLM
 
-dataiku.set_remote_dss("http://localhost:11000/", os.environ.get("DKU_API_KEY"))
 default_calendar_id = "rosenthal.remi@gmail.com"
 llm_model_id = "openai:OpenAI:gpt-4o"
-prompt = """Plan a surftrip to Limoges"""
+dataiku.set_remote_dss("http://localhost:11000/", os.environ.get("DKU_API_KEY"))
 
 
-if __name__ == '__main__':
-    import dataiku
-    client = dataiku.api_client()
-    project = client.get_project("AGENTSURFTRIPPLANNER")
-    dataset = project.get_dataset("conversations")
-    print(dataset.get_as_core_dataset().get_dataframe())
-    print(test)
-    dku_client = dataiku.api_client()
-    model = DKUChatLLM(llm_id=llm_model_id)
-    agent_builder = AgentBuilder(
-         client=dku_client,
-         model=model,
-         calendar_id=default_calendar_id
-     )
-    print(prout)
-    agent = agent_builder.build()
-    inputs = {"messages": [HumanMessage(content=prompt)]}
-    final_state = agent.invoke(inputs)
-    final_answer = final_state["messages"][-1].content
-    print(final_answer)
+class MyLLM(BaseLLM):
+
+    def __init__(self):
+        dku_client = dataiku.api_client()
+        model = DKUChatLLM(llm_id=llm_model_id)
+        agent_builder = AgentBuilder(
+            client=dku_client,
+            model=model,
+            calendar_id=default_calendar_id
+        )
+        self.agent = agent_builder.build()
+
+    def process(self, query, settings, trace):
+        """
+        Handles the full conversational turn.
+        """
+        tracer = LangchainToDKUTracer(dku_trace=trace)
+
+        langchain_messages = []
+        for msg in query["messages"]:
+            if msg.get("role") == "assistant":
+                langchain_messages.append(AIMessage(**msg))
+            else:
+                langchain_messages.append(HumanMessage(**msg))
+
+        inputs = {
+            "messages": langchain_messages,
+        }
+
+        final_state = self.agent.invoke(inputs)
+        final_answer_message = final_state["messages"][-1]
+        return {"text": final_answer_message.content}
+
+
+my_llm = MyLLM()
+print(my_llm.process({"messages": [{
+    "content": "I'd like to plan a surf trip to Bayonne. I am departing from Paris. I'd like to go on the 2 November 2025 and return on the 10 November 2025.",
+    "role": "user"}]},
+    None, None))
